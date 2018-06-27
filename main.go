bb9597a9c5325931986c9e96d28bb757ec053e21
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-06-27 16:24:35 +0200

    Delete files one by one

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
 
 func main() {
-	argsWithoutProg := os.Args[1:]
-	for _, dir := range argsWithoutProg {
-		os.RemoveAll(dir)
-		os.MkdirAll(dir, 0777)
+	dirs := os.Args[3:]
+	loading, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid loading: ", err)
+		return
+	}
+	total, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid total: ", err)
+		return
+	}
+	for _, dir := range dirs {
+		files, err := filepath.Glob(dir + "/*")
+		if err != nil {
+			panic(err)
+		}
+		for i, f := range files {
+			if i%loading == 0 {
+				fmt.Print("#")
+			}
+			if (i+1)%total == 0 {
+				break
+			}
+			if err := os.Remove(f); err != nil {
+				panic(err)
+			}
+		}
 	}
 }
