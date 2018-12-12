e8952da68e40711a575dd3eccb49b381fc49ba05
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-12-12 10:43:50 +0100

    use the flag library with default values

--- main.go
+++ main.go
@@ -1,32 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 )
 
 func main() {
-	dirs := os.Args[4:]
+	total := flag.Int("nb", 0, "number of files to delete (0 to delete every file)")
+	workers := flag.Int("w", 10, "number of concurrent workers")
+	flag.Parse()
+	dirs := flag.Args()
 
-	loading, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("Invalid loading: ", err)
-		return
-	}
-
-	total, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		fmt.Println("Invalid total: ", err)
-		return
-	}
-
-	workers, err := strconv.Atoi(os.Args[3])
-	if err != nil {
-		fmt.Println("Invalid workers: ", err)
-		return
-	}
+	fmt.Printf("deleting %d folders with %d concurrent workers\n", len(dirs), (*workers))
 
 	for _, dir := range dirs {
 		paths, err := filepath.Glob(dir + "/*")
@@ -34,18 +21,21 @@ func main() {
 			panic(err)
 		}
 
-		total := total
+		// use a "local" total for the current dir, that can be reassigned
+		total := (*total)
 
-		if total > len(paths) {
+		if total > len(paths) || total == 0 {
 			total = len(paths)
 		}
 
-		fmt.Printf("deleting %v out of %v files in %s with %v workers\n", total, len(paths), dir, workers)
+		// fmt.Printf("deleting %d out of %d files in %s with %d workers\n", total, len(paths), dir, (*workers))
+		fmt.Print(dir)
+		fmt.Print(" ")
 
 		files := make(chan string, total)
 		done := make(chan bool, total)
 
-		for i := 0; i < workers; i++ {
+		for i := 0; i < (*workers); i++ {
 			go worker(files, done)
 		}
 
@@ -53,6 +43,11 @@ func main() {
 			files <- paths[i]
 		}
 		close(files)
+
+		loading := (total / 10)
+		if loading == 0 {
+			loading = 1
+		}
 
 		for i := 0; i < total; i++ {
 			<-done
@@ -66,13 +61,11 @@ func main() {
 }
 
 func worker(files <-chan string, done chan<- bool) {
-	i := 0
 	for f := range files {
 		if err := os.Remove(f); err != nil {
 			done <- false
 			continue
 		}
-		i++
 		done <- true
 	}
 }
