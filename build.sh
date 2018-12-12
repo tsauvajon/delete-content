c2fab989bdb7acac003712ad7acf0f8fa566d34f
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-12-12 16:25:10 +0100

    clean files before and after building

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 export GOARCH=amd64 CGO_ENABLED=0
 
+[ -e delete-content-*.zip ] && rm delete-content-*.zip
+[ -e delete-content ] && rm delete-content
+
+echo "building for linux64"
 GOOS=linux go build -o delete-content
 zip delete-content-linux64.zip delete-content
 
+rm delete-content
+
+echo "building for macOS64"
 GOOS=darwin go build -o delete-content
 zip delete-content-macos64.zip delete-content
 
 rm delete-content
 
+echo "building for windows64"
 GOOS=windows go build -o delete-content.exe
 zip delete-content-windows64.zip delete-content.exe
 
